a04026e08fac2ee668ff96b64b34654713fb8620
Kristin Beltz <beltz08@me.com>
2015-08-17 14:14:33 -0500

    Add method to list five most common responses as checkbox inputs for Ethnicity.

--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -1,38 +1,39 @@
 class UserAnswer < ActiveRecord::Base
   attr_accessible :user_id, :answer_id, :answer_type, :value
-  
+
   belongs_to :answer, polymorphic: true
-  
+
   belongs_to :user
-  
+
   # accepts_nested_attributes_for :answer
-  
+
   def field_value
     self.answer_type
   end
-  
+
   def value
     self.answer.value if self.answer
   end
-  
+
   def value=(form_value)
     answer_object = self.answer_type.constantize.find_or_create_by_value(form_value)
     self.answer_id = answer_object.id
   end
-  
+
   def single_answer_question?
     UserAnswer.single_answer_types.include?(self.answer_type)
   end
-  
+
   def self.answer_types
     single_answer_types + multi_answer_types
   end
-  
+
   def self.single_answer_types
     ["Age", "Education", "Income"]
   end
-  
+
   def self.multi_answer_types
     ["Ethnicity", "Gender", "Orientation"]
   end
+
 end
