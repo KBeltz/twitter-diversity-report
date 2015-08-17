11631f97e24fd89acba8068ef3c850ab87151270
Kristin Beltz <beltz08@me.com>
2015-08-17 14:22:25 -0500

    Add methods to list five most common responses as checkbox inputs for Gender and Orientation.

--- app/models/orientation.rb
+++ app/models/orientation.rb
@@ -1,7 +1,21 @@
 class Orientation < ActiveRecord::Base
   attr_accessible :value
 
-  validates :value, presence: true  
-  
+  validates :value, presence: true
+
   has_many :user_answers, as: :answer
+
+  #returns five most common values to populate checkbox fields in user add/edit form
+  def self.top_five
+
+    query = self.select(:value).uniq
+    hash_of_answers = {}
+    query.each do |v|
+      a = self.where(value: v.value).count
+      hash_of_answers[v] = a
+      # binding.pry
+    end
+    #binding.pry
+    return hash_of_answers.sort_by(&:last).reverse.take(5)
+  end
 end
