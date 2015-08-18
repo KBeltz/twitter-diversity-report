59fb9f3be9baed2332374d819f967a41ffa02346
Kristin Beltz <beltz08@me.com>
2015-08-18 10:09:04 -0500

    Removed binding.pry

--- app/models/ethnicity.rb
+++ app/models/ethnicity.rb
@@ -7,15 +7,12 @@ class Ethnicity < ActiveRecord::Base
 
   #returns five most common values to populate checkbox fields in user add/edit form
   def self.top_five
-
     query = self.select(:value).uniq
     hash_of_answers = {}
     query.each do |v|
       a = self.where(value: v.value).count
       hash_of_answers[v] = a
-      # binding.pry
     end
-    #binding.pry
     return hash_of_answers.sort_by(&:last).reverse.take(5)
   end
 
